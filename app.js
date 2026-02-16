af5b0651a3fd2fd0504ba2ef6cb709529d54b558
Matthew Baker <matthewbaker13@hotmail.co.uk>
2026-02-16 17:45:03 +0000

    Add dom html to js. Clear text and append form field text to a h2

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //https://pokeapi.co/api/v2/pokemon/
+const searchForm = document.getElementById('searchForm');
+const textInput = document.getElementById('textInput');
+const button = document.getElementById('searchButton');
+const pokemonName = document.getElementById('pokemonName');
+
+searchForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    pokemonName.textContent = "";
+    const text = textInput.value;
+    pokemonName.append(text);
+});
