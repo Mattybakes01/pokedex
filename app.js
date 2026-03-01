050349def3b0b2f1590db9fd1d0170fb1b88c09e
Matthew Baker <matthewbaker13@hotmail.co.uk>
2026-03-01 18:49:32 +0000

    Get pokemon name and sprite and append on body when searching by ID

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-//https://pokeapi.co/api/v2/pokemon/
 const searchForm = document.getElementById('searchForm');
 const textInput = document.getElementById('textInput');
 const pokemonName = document.getElementById('pokemonName');
+const image = document.getElementById("image")
 
 const getPokeApi = async (id) => {
     try {
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-        console.log(res.data);
+        const config = { headers: {Accept:'application/json'} };
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        console.log(res.data.name);
+        
+        //Gets name and appends to H2
+        const text = res.data.name
+        pokemonName.append(text);
+        
+        //Gets artwork and appends to body
+        const artwork = res.data.sprites.other['official-artwork'].front_default;
+        image.src = artwork;
     } catch (e) {
         console.log("Error", e);
     }
 };
 
+
+
 searchForm.addEventListener('submit', function(e) {
     e.preventDefault();
     pokemonName.textContent = "";
-    const text = textInput.value;
-    pokemonName.append(text);
-    textInput.value = "";
-    getPokeApi(text);
+    image.src = "";
+    const id = parseInt(textInput.value.trim(), 10);
+    getPokeApi(id);
+    textInput.value = "";    
 });
+
+
